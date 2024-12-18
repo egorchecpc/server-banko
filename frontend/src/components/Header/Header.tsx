fcffe297d9262bcc71cecfc5c9272f1f0f9366db
egorchecpc <egorchecpc@gmail.com>
2024-12-18 14:17:42 +0300

    [Banko. Test commit

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -16,6 +16,7 @@ export const Header: FC<HeaderProps> = ({ navItems, userData }) => {
         <div className="flex items-center gap-3">
           <SidebarTrigger />
           <Navbar navItems={navItems} />
+          <div>Hello!</div>
         </div>
         <div className="flex gap-5">
           <ImportComponent />
