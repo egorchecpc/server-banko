fcb617603f0868dd674021d382aaf124c1a47918
egorchecpc <egorchecpc@gmail.com>
2024-12-27 11:47:11 +0300

    [Banko] tests

--- frontend/src/services/axiosConfig.ts
+++ frontend/src/services/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosConfig = axios.create({
-  baseURL: import.meta.env.VITE_APP_BASE_URL,
+  baseURL: 'http://localhost:3000/', //import.meta.env.VITE_APP_BASE_URL
   headers: {
     'Content-Type': 'application/json',
   },
