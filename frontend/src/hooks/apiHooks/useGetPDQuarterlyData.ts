91575312900eee686ca0e1069a134025803ed9b8
egorchecpc <egorchecpc@gmail.com>
2024-12-27 11:38:41 +0300

    [Banko] update endpoints

--- frontend/src/hooks/apiHooks/useGetPDQuarterlyData.ts
+++ frontend/src/hooks/apiHooks/useGetPDQuarterlyData.ts
@@ -1,12 +1,13 @@
 import { useQuery } from '@tanstack/react-query'
 import { QuarterlyDataResponse } from '@/models/PD'
 import axiosConfig from '@/services/axiosConfig'
+import axios from 'axios'
 
 export const useGetPDQuarterlyData = () => {
   return useQuery<QuarterlyDataResponse, Error>({
     queryKey: ['PDQuarterlyData'],
     queryFn: async () => {
-      const { data } = await axiosConfig.get('/probabilityDefault/quarterly')
+      const { data } = await axios.get('api/probabilityDefault/quarterly')
       return data
     },
   })
