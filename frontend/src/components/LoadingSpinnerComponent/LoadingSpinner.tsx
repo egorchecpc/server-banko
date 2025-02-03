6eb30ae5323161dc55d8c787a943864ba1351cb3
egorchecpc <egorchecpc@gmail.com>
2025-02-03 14:44:39 +0300

    update spinner

--- frontend/src/components/LoadingSpinnerComponent/LoadingSpinner.tsx
+++ frontend/src/components/LoadingSpinnerComponent/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ import loaderGif from './loader.gif'
 
 const LoadingSpinner = () => {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center bg-grey-300/40">
+    <div className="flex min-h-screen w-full items-center justify-center">
       <div className="flex flex-col items-center gap-4">
         {/*<div className="border-primary h-12 w-12 animate-spin rounded-full border-4 border-t-transparent" />*/}
         <img src={loaderGif} alt="Loading..." className="h-14 w-14" />
