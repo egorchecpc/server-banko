38e7cc863c518251d45ad07c64aa2d5a4dac5b61
egorchecpc <egorchecpc@gmail.com>
2025-02-11 16:40:57 +0300

    fix tooltip in ECL module

--- frontend/src/modules/ECLDisplayModule/ECLDisplayModule.tsx
+++ frontend/src/modules/ECLDisplayModule/ECLDisplayModule.tsx
@@ -16,19 +16,20 @@ import {
 import { GearIcon } from '@radix-ui/react-icons'
 import { Switch } from '@/components/ui/switch'
 import { Link } from '@tanstack/react-router'
-import { Button } from '@/components/ui/button'
 
 interface ECLDisplayModuleProps {
   eclDataV1: ECLData
   eclDataV2: ECLData
   reportId: string
 }
+
 const ECLDisplayModule: FC<ECLDisplayModuleProps> = ({
-  eclDataV1,
-  eclDataV2,
-  reportId,
-}) => {
+                                                       eclDataV1,
+                                                       eclDataV2,
+                                                       reportId,
+                                                     }) => {
   const [showDelta, setShowDelta] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false)
 
   const queryClient = useQueryClient()
   const { data: eclDiff1 } = useQuery({
@@ -43,65 +44,68 @@ const ECLDisplayModule: FC<ECLDisplayModuleProps> = ({
   })
 
   const handleSwitchChange = () => {
-    setShowDelta((prevShowDelta) => !prevShowDelta)
-    toast.info(
-      'Обратите внимание, что для отображения ECL разниц, необходимо ввести новые макропоказатели.'
-    )
+    setShowDelta((prev) => !prev)
+    setMenuOpen(false)
+    if (!eclDiff1 || !eclDiff2) {
+      toast.info(
+          'Обратите внимание, что для отображения ECL разниц, необходимо ввести новые макропоказатели.'
+      )
+    }
   }
 
   return (
-    <ContainerComponent withBg={true}>
-      <ContainerHeader>
-        <div className="flex w-full justify-between">
-          <div className="flex items-center">
-            <div className="text-xl font-bold leading-24 text-black-800">
-              Ожидаемые кредитные убытки (ОКУ, ECL)
+      <ContainerComponent withBg={true}>
+        <ContainerHeader>
+          <div className="flex w-full justify-between">
+            <div className="flex items-center">
+              <div className="text-xl font-bold leading-24 text-black-800">
+                Ожидаемые кредитные убытки (ОКУ, ECL)
+              </div>
+              <DropdownMenu open={menuOpen} onOpenChange={setMenuOpen}>
+                <DropdownMenuTrigger asChild>
+                  <button className="!ring-none rounded-full px-2 py-1 hover:bg-gray-200">
+                    <GearIcon />
+                  </button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                    side="right"
+                    className="flex items-center gap-2 p-2"
+                >
+                  <div className="text-[14px]">Отображение разниц</div>
+                  <Switch
+                      checked={showDelta}
+                      onCheckedChange={handleSwitchChange}
+                  />
+                </DropdownMenuContent>
+              </DropdownMenu>
             </div>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <button className="!ring-none rounded-full px-2 py-1 hover:bg-gray-200">
-                  <GearIcon />
-                </button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent
-                side="right"
-                className="flex items-center gap-2 p-2"
-              >
-                <div className="text-[14px]">Отображение разниц</div>
-                <Switch
-                  checked={showDelta}
-                  onCheckedChange={handleSwitchChange}
-                />
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <div>
+              <Link to={`/reports/${reportId}/credit-list`}>
+                <p className="mr-4 bg-transparent text-[14px] text-blue-900 hover:text-charts-60">
+                  См. все кредиты
+                </p>
+              </Link>
+            </div>
           </div>
-          <div>
-            <Link to={`/reports/${reportId}/credit-list`}>
-              <p className="mr-4 bg-transparent text-[14px] text-blue-900 hover:text-charts-60">
-                См. все кредиты
-              </p>
-            </Link>
-          </div>
-        </div>
-      </ContainerHeader>
-      <ContainerBody isScrolling={true} orientation="horizontal">
-        <ECLTable
-          data={eclDataV1}
-          isFirst={true}
-          eclDiff={eclDiff1 as ECLData}
-          showDelta={showDelta}
-        />
-      </ContainerBody>
-      <div className="my-4"></div>
-      <ContainerBody isScrolling={true} orientation="horizontal">
-        <ECLTable
-          data={eclDataV2}
-          isFirst={false}
-          eclDiff={eclDiff2 as ECLData}
-          showDelta={showDelta}
-        />
-      </ContainerBody>
-    </ContainerComponent>
+        </ContainerHeader>
+        <ContainerBody isScrolling={true} orientation="horizontal">
+          <ECLTable
+              data={eclDataV1}
+              isFirst={true}
+              eclDiff={eclDiff1 as ECLData}
+              showDelta={showDelta}
+          />
+        </ContainerBody>
+        <div className="my-4"></div>
+        <ContainerBody isScrolling={true} orientation="horizontal">
+          <ECLTable
+              data={eclDataV2}
+              isFirst={false}
+              eclDiff={eclDiff2 as ECLData}
+              showDelta={showDelta}
+          />
+        </ContainerBody>
+      </ContainerComponent>
   )
 }
 
