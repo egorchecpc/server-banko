cb1e063ec61e9b66875e091c61079936997900ab
egorchecpc <egorchecpc@gmail.com>
2025-03-14 15:28:34 +0700

    change style from container component

--- frontend/src/components/ContainerComponent/ContainerComponent.tsx
+++ frontend/src/components/ContainerComponent/ContainerComponent.tsx
@@ -14,7 +14,7 @@ const ContainerComponent: FC<ContainerComponentProps> = ({
   return (
     <div
       className={
-        withBg ? 'h-full w-full rounded-lg border bg-grey-300/40 p-1.5' : ''
+        withBg ? 'h-full w-full rounded-lg border border-grey-900/60 shadow-2xl p-1.5' : ''
       }
     >
       {title ? (
